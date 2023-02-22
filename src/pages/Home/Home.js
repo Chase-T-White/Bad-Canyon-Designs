2b4c503d6f253f9a2311b6d3eb689cbdf6c95526
cwhite70 <chase.t.white@hotmail.com>
2023-02-21 22:07:50 -0700

    more picture and collage started on Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
 import "./home.css";
 
 const Home = () => {
@@ -9,13 +12,15 @@ const Home = () => {
     <main className="home">
       {/* <div className={isLoad ? "preloader hidden" : "preloader"}></div> */}
       <Navbar bg="light">
-        <Container>
-          <Nav className="me-auto">
+        <Container className="justify-content-center">
+          <Nav>
             <Nav.Link>Shop</Nav.Link>
             <Nav.Link>Gallery</Nav.Link>
           </Nav>
-          <Navbar.Brand>Bad Canyon Designs</Navbar.Brand>
-          <Nav className="me-auto">
+          <Navbar.Brand className="nav-logo mx-5">
+            Bad Canyon Designs
+          </Navbar.Brand>
+          <Nav>
             <Nav.Link>Story</Nav.Link>
             <Nav.Link>Studio</Nav.Link>
             <Nav.Link>Cart</Nav.Link>
@@ -23,7 +28,46 @@ const Home = () => {
         </Container>
       </Navbar>
       <section className="hero">
-        <h1 className="hero_title">The Art of Nick White</h1>
+        <Container>
+          <h1 className="hero_title text-center pt-5 text-light">
+            The Art of Nick White
+          </h1>
+          <Row>
+            <Col>
+              <Image fluid src="../../assets/homePics/heroTopA.jpg"></Image>
+            </Col>
+            <Col>
+              <Image
+                fluid
+                src="../../assets/homePics/heroTopB.jpg"
+                className="h-100"
+                style={{ objectFit: "cover" }}
+              ></Image>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Image fluid src="../../assets/homePics/heroAsideA.jpg"></Image>
+            </Col>
+            <Col>
+              <Col>
+                <Image
+                  fluid
+                  src="../../assets/homePics/heroMiddleA.jpg"
+                ></Image>
+              </Col>
+              <Col>
+                <Image
+                  fluid
+                  src="../../assets/homePics/heroMiddleB.jpg"
+                ></Image>
+              </Col>
+            </Col>
+            <Col>
+              <Image fluid src="../../assets/homePics/heroAsideB.jpg"></Image>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </main>
   );
